162bbaacaf842bb17d1d472015487c8878d28cf3
Johannes <js@unsyn.com>
2020-06-04 18:03:19 +0200

    Added mapping and project specific setting of abstract.

--- public/js/editor-modal.js
+++ public/js/editor-modal.js
@@ -72,11 +72,31 @@ apos.define('apostrophe-crossref-editor-modal', {
               .addClass('pl-success')
               .find('.apos-field-label').text(enhanceTargetLabel)
 
+            // TODO this is project specific code. likely move to a hook defined at runtime
+            // before mappings, fire a publication type change to show/hide the proper form fields
+            $('select[name=publicationType]')
+                         .find('option[value=' + response.body.publicationType + ']')
+                         .prop('selected',true)
+                         .trigger('change');
+
             Object.keys(options.crossrefConfig.mapFields).map(function (key) {
               self.$el.find('[name="'+ key +'"]')
                 .val(response.body[options.crossrefConfig.mapFields[key]])
                 .addClass(animationClass)
             });
+
+            // TODO this is project specific code. likely move to a hook defined at runtime
+            // after mappings, handle setting rich text field with the abstract
+            if(response.body.journalAbstract) {
+              if($('[data-richabstract-edit-view] [data-rich-text]').length) {
+                // text field for abstract is present, just edit it
+                $('[data-richabstract-edit-view] [data-rich-text]').html(response.body.journalAbstract);
+              } else {
+                // need to create a text field for abstract, and set a timeout
+                $('[data-richabstract-edit-view] button[data-apos-add-item]').click();
+                setTimeout(() => $('[data-richabstract-edit-view] [data-rich-text]').html(response.body.journalAbstract), 500);
+              }
+            }
 
             // set slug on its own
             self.$el.find('[name="slug"]')
